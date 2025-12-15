2ab3964f60acf9ab1b1f1f6645310411e2769a99
Josh Wilson <joshwilsonvu@gmail.com>
2025-12-14 22:28:02 -0500

    Style updates, make the root a proper index.

--- astro.config.ts
+++ astro.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 import { findAndReplace } from "hast-util-find-and-replace";
@@ -8,6 +8,9 @@ import { formatters } from "./src/utils";
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+    server: {
+      host: true,
+    },
   },
   scopedStyleStrategy: "class",
   devToolbar: {
@@ -21,6 +24,26 @@ export default defineConfig({
       () => (tree: any) => {
         findAndReplace(tree, formatters);
         return tree;
+      },
+    ],
+  },
+  // make recipes available at top level
+  redirects: {
+    "/[...id]": "/recipes/[...id]",
+  },
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        // also considered "Shadows Into Light Two", "Kalam", "Caveat Brush"
+        name: "Beth Ellen",
+        cssVariable: "--font-beth-ellen",
+      },
+      {
+        provider: fontProviders.google(),
+        // also considered "Poppins"
+        name: "Lato",
+        cssVariable: "--font-lato",
       },
     ],
   },
